0bae7fb8e39a0d4e8b400999a3ffe37a8cb3f6bf
NoviarPutra <noviarpb@gmail.com>
2024-06-01 14:32:58 +0700

    remove pnpm

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,3 @@
-// import type { Application } from "express";
-// import authModules from "./authentication/authentication.route";
-
-// const authRoute = (app: Application) => {
-//   app.use("/api/authentication", authModules);
-// };
-
-// export default authRoute;
 const authModules = require("./authentication/authentication.route");
 module.exports = (app) => {
   app.use("/api/authentication", authModules);
